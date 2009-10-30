8279603fd2bbd4b663261e0a0d2113af72cc615f
Webb Sprague <wsprague@foobar.(none)>
2009-10-29 21:35:33 -0700

    Small changes to citab-test.R

--- demography/r-functions/citab/citab-test.R
+++ demography/r-functions/citab/citab-test.R
@@ -1,20 +1,27 @@
 
+## Get function and sample data
 source('citab.R')
-load('testdata.RData')
+load('testdata.RData')                  #this loads pums.mex
 
-# Simple tabulation with error
+## Begin
+print('Starting citab-test.R')
+
+## Simple tabulation with error
 print (citab(varname='SEX', x=pums.mex))
 
-# Two-way tabulation with error
+## Two-way tabulation with error
 citab(varname='PUMA+SEX', x=pums.mex)
 
-# Simple tabulation with error and subset
+## Simple tabulation with error and subset
 print (citab(varname='SEX', x=pums.mex, subset=(pums.mex$AGEP==16)))
 
-# Two-way tabulation with error and subset
+## Two-way tabulation with error and subset
 print (citab(varname='SEX+AGEP', x=pums.mex, subset=(pums.mex$AGEP>=16 & pums.mex$AGEP<=21)))
 
-# Test error
+## Test error
+print('Following this message you should see an error due to bad CI.')
 try(citab(varname='SEX+AGEP', x=pums.mex, ci=1.05))
-print ('finished')
+
+## Finis
+print ('\nFinished with citab-test.R')
 
