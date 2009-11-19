9890da3b1fa5038a0d0de49280aae9eece326c76
Your Name <webb.sprague@gmail.com>
2009-11-18 17:33:52 -0800

    Calculate rates fine, still working on graphs.

--- demography/r-functions/agespecv/agespecv-test.R
+++ demography/r-functions/agespecv/agespecv-test.R
@@ -4,5 +4,11 @@ pums.mex$FER.LOGIC = pums.mex$FER==1    # baby in last year?
 pums.mex$MIL.LOGIC = pums.mex$MIL==1    # active service?
 pums.mex$MIL.LOGIC = pums.mex$MIG>=2    # any move in last year (both domestica and international)
 pums.mex$MAR.LOGIC = pums.mex$MAR<=4    # ever married?
-print(agespecv(data=pums.mex, agev='AGEF.EQUAL', weightv='pwgtp', eventv='FER.LOGIC'))
+
+pums.mex$MARF = factor(pums.mex$MAR, labels = c('mar', 'wid', 'div', 'sep', 'nev'))
+
+print(agespecv(data=pums.mex, agev='AGEF.EQUAL', weightv='pwgtp', eventv='FER.LOGIC', filename='~/public_html/foo1.pdf'))
+print(agespecv(data=pums.mex, agev='AGEF.EQUAL', weightv='pwgtp', eventv='MAR.LOGIC', partv='SEX', filename='~/public_html/foo2.pdf'))
+print(agespecv(data=pums.mex, agev='AGEF.EQUAL', weightv='pwgtp', eventv='FER.LOGIC', partv='MARF', filename='~/public_html/foo2.pdf'))
+
 rm("pums.mex")
--- demography/r-functions/agespecv/agespecv.R
+++ demography/r-functions/agespecv/agespecv.R
@@ -5,11 +5,79 @@
 ## Function to graph age- and sex- specific rates from PUMS data using an event
 ## variable and a partitioning variable
 
-## input parameters
-##    data
-##    eventv=NULL  If empty, calculates proportion of population 
-##    partv=NULL  If empty, doesn't partition
-##    file=NULL  If empty, uses default displays
+## input parameters -- except for data, all string names for columns in data
+
+##    data -- Data frame
+
+##    agev -- Ordered.factor along which to calculate rates
+
+##    weightv -- weights for records
+
+##    eventv=NULL -- Boolean variable of whether event happened or not.  These
+##    are counted up for the numberator. If NULL, calculates proportion of
+##    population
+
+##    partv=NULL -- Factor variable by which to partition the calculations and
+##    graphs. If NULL, doesn't partition
+
+##    period=1 -- multiplier for fractional periods (might be 1.5 for 18 months
+##    and yearly rates, for ex)
+
+##    filename=NULL -- output filename. If empty, uses default displays
+
+## Weidnesses:
+
+##    xtabs() ignores Na's and NULLs, so when we calculate rates with it, if the
+## these invalid results are not even part of the universe
+
+## xyplot(Freq~AGEF.EQUAL, as.data.frame(prop.table(xtabs(pwgtp~AGEF.EQUAL+SEX,
+## subset=DMV.MEX))), groups=SEX, type='l')
+
+agespecv = function (data, agev, weightv=NULL, eventv=NULL, partv=NULL, period=1, filename=NULL) {
+
+  require(ggplot2)
+  require(lattice)
+  
+  ## calc rates
+  if (is.null(partv) & is.null(eventv)) { # age dist, no groups
+    ;
+  } else if (is.null(partv) & !is.null(eventv)) { #age spec rate, no groups
+    eventsf = as.formula(sprintf('%s ~ %s + %s', weightv, eventv, agev))
+    events = xtabs(eventsf, data)         #see note on xtabs() above 
+    num = events['TRUE',]
+  } else if (!is.null(partv) & is.null(eventv)){ # age dist, groups
+    ;
+  } else if (!is.null(partv) & !is.null(eventv)) { #age spec rate, groups
+    eventsf = as.formula(sprintf('%s ~ %s + %s + %s', weightv, eventv, agev, partv));
+    events = xtabs(eventsf, data)
+    num = events['TRUE',,]
+  }
+  den = colSums(events)*period 
+  rates = num / den
+  ratesdf = as.data.frame(rates)
+  ratesdf$age = levels(data[[agev]])
+  
+  return(list(num=num, den=den, rates=rates, ratesdf=ratesdf))  
+  
+  ## make graph
+  if (! is.null(filename)){
+    pdf(filename)
+    g = ggplot() +
+      layer(data = ratesdf, mapping = aes(x = age, y = rates), geom = "point", stat="identity") +
+      layer(data = ratesdf, mapping = aes(x = age, y = rates), geom = "smooth", stat = "smooth", method = loess)
+    print(g)
+    dev.off()
+  } else {
+    g = plot( ratesdf$rates)
+    print(g)
+  }
 
 
-## xyplot(Freq~AGEF.EQUAL, as.data.frame(prop.table(xtabs(pwgtp~AGEF.EQUAL+SEX, subset=DMV.MEX))), groups=SEX, type='l')
+  
+}
+
+
+
+
+
+  
